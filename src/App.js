2b5412b00f9a5e1b82affcd6da4c3175b377bc6c
arun496-pep <99860605+arun496-pep@users.noreply.github.com>
2022-02-17 11:46:36 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, by Dan Abramov!!!
+          Learn React, by Dan Abramov:)
         </a>
       </header>
     </div>
